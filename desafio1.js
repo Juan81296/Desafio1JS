47f01c53bf3c28488cf934ac9f62cc1b9e891f83
Juan Tenaglia <juantenaglia81296@gmail.com>
2021-10-26 13:48:20 -0300

    Desafio1

---
+++ desafio1.js
@@ -0,0 +1,12 @@
+let nombreUsuario = prompt("Ingrese su usuario");
+let edad = prompt("Ingrese su edad");
+
+alert("Bienvenido " + nombreUsuario);
+alert("Tu edad es: " + edad)
+
+let numero1 = prompt("Ingrese un número");
+let numero2 = prompt("Ingrese otro número");
+
+let suma = parseInt(numero1) + parseInt(numero2);
+
+alert("La suma de los numeros da: " + suma);
